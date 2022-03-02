e977128c897c93f1b0443573d038fdd1793dbaff
samirbenbouker <samirbenbouker@gmail.com>
2022-03-02 23:54:59 +0100

    Create Whatssap app MERN, create Sidebar and Chat component and import firebase config

---
+++ public/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDOzgIz_J-SHi-GnQbv5ZKAn_0tn9BTCkQ",
+    authDomain: "whatssap-app-mern.firebaseapp.com",
+    projectId: "whatssap-app-mern",
+    storageBucket: "whatssap-app-mern.appspot.com",
+    messagingSenderId: "498271214797",
+    appId: "1:498271214797:web:b54d4e95778c936a5fdd84",
+    measurementId: "G-KNKTV9GHBH"
+};
